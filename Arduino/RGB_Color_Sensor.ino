344aac816575e986ed173c9918049491da2f93cb
SarasSankhe <sarassankhe69@gmail.com>
2026-07-16 21:48:15 +0530

    Implement watchdog timer in RGB color sensor code
    Added watchdog timer functionality to monitor the program and prevent hangs.

--- Arduino/RGB_Color_Sensor.ino
+++ Arduino/RGB_Color_Sensor.ino
@@ -1,8 +1,19 @@
 #include <Arduino_APDS9960.h>
+#include <mbed.h>
 
+using namespace mbed;
+
+// RGB values
 int r = 0, g = 0, b = 0;
 
+// Watchdog instance
+Watchdog &watchdog = Watchdog::get_instance();
+
+// Counter used only to demonstrate WDT reset
+int counter = 0;
+
 void setup() {
+
   Serial.begin(9600);
 
   while (!Serial);
@@ -13,16 +24,22 @@ void setup() {
   }
 
   Serial.println("APDS9960 Ready");
+
+  // Start Watchdog with 4-second timeout
+  watchdog.start(4000);
 }
 
 void loop() {
 
-  // Update values only if a new reading is available
+  // Refresh the watchdog
+  watchdog.kick();
+
+  // Read sensor values
   if (APDS.colorAvailable()) {
     APDS.readColor(r, g, b);
   }
 
-  // Always send the latest values
+  // Send values to Python
   Serial.print("R:");
   Serial.print(r);
   Serial.print(",G:");
@@ -31,7 +48,27 @@ void loop() {
   Serial.println(b);
 
   delay(200);
+
+  counter++;
+
+  // After about 20 seconds, simulate a software hang
+  if (counter == 100) {
+
+    Serial.println("Simulating Program Hang...");
+    Serial.println("Watchdog will reset the board in 4 seconds.");
+
+    while (1) {
+      // Do nothing
+      // watchdog.kick() is NOT called
+      // WDT will reset the board automatically
+    }
+  }
 }
+
+
+
+
+
 
 
 
